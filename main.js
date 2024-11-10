d7f2a8807a268ba984c5d4e053f89dd7b18bb1c4
Mohammed <mdjaveed9108@gmail.com>
2024-11-10 21:22:07 +0000

    created function to list down routes and styles added

--- main.js
+++ main.js
@@ -13,6 +13,31 @@ function addRoute(){
     if(!startPoint||!destination||!routeName||!description){
         alert("Please fill all the required fields ")
     }else{
-    console.log("success")
+
+   // Creating  list  to display the route
+   const routesContainer = document.getElementById("routesContainer");
+   const listContainer = document.createElement("li");
+   
+   // Formatting the route details
+   listContainer.innerHTML = `
+       <strong>Route Name:</strong> ${routeName} <br>
+       <strong>Starting Point:</strong> ${startPoint} <br>
+       <strong>Destination:</strong> ${destination} <br>
+       <strong>Description:</strong> ${description}
+       <Button id ="editButton">Edit</Button>
+        <Button id="deleteButton">Delete</Button><br>
+    <strong>-----------------------------------------------------------------------------------------------------------</strong>
+   `;
+   
+   // Adding the new route to the list
+   routesContainer.appendChild(listContainer);
+
+   // To Clear field
+   document.getElementById("Starting_Point").value = '';
+   document.getElementById("destination").value = '';
+   document.getElementById("routeName").value = '';
+   document.getElementById("routeDescription").value = '';
+   
+   console.log("Route added successfully");
     }
 }
